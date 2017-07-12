5ad1c93d1c70289e9138a43a99afd60cf7f1206d
Zyrah Bernardino <zyrahbernardino@gmail.com>
2017-07-13 08:31:21 +1000

    Initial use of KO
    Got KO to work!

--- js/app.js
+++ js/app.js
@@ -1,51 +1,66 @@
-  var map;
+var map;
+// Create a new blank array for all the listing markers.
+var markers = [];
 
-  // Create a new blank array for all the listing markers.
-  var markers = [];
-
-  function initMap() {
-    // Constructor creates a new map - only center and zoom are required.
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 40.7413549, lng: -73.9980244},
-      zoom: 13,
-      mapTypeControl: false
-    });
-
+/* ======= Model ======= */
     // These are the real estate listings that will be shown to the user.
     // Normally we'd have these in a database instead.
-    var locations = [
-      {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
-      {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
-      {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
-      {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
-      {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
-      {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
-    ];
+var locations = [
+  {title: 'Park Ave Penthouse', coordinates: {lat: 40.7713024, lng: -73.9632393}},
+  {title: 'Chelsea Loft', coordinates: {lat: 40.7444883, lng: -73.9949465}},
+  {title: 'Union Square Open Floor Plan', coordinates: {lat: 40.7347062, lng: -73.9895759}},
+  {title: 'East Village Hip Studio', coordinates: {lat: 40.7281777, lng: -73.984377}},
+  {title: 'TriBeCa Artsy Bachelor Pad', coordinates: {lat: 40.7195264, lng: -74.0089934}},
+  {title: 'Chinatown Homey Space', coordinates: {lat: 40.7180628, lng: -73.9961237}}
+];
 
-    var largeInfowindow = new google.maps.InfoWindow();
 
-    // The following group uses the location array to create an array of markers on initialize.
-    for (var i = 0; i < locations.length; i++) {
-      // Get the position from the location array.
-      var position = locations[i].location;
-      var title = locations[i].title;
-      // Create a marker per location, and put into markers array.
-       var marker = new google.maps.Marker({
-        position: position,
-        title: title,
-        animation: google.maps.Animation.DROP,
-        id: i
-      });
-      // Push the marker to our array of markers.
-      markers.push(marker);
-      // Create an onclick event to open an infowindow at each marker.
-      marker.addListener('click', function() {
-        populateInfoWindow(this, largeInfowindow);
-      });
-    }
-    document.getElementById('show-listings').addEventListener('click', showListings);
-    document.getElementById('hide-listings').addEventListener('click', hideListings);
+var places = ko.observableArray(locations);
+
+function MapViewModel() {
+    var self = this;
+    // Editable data
+    self.places = places;
+
+}
+
+ko.applyBindings(new MapViewModel());
+
+function initMap() {
+  // Constructor creates a new map - only center and zoom are required.
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 40.7413549, lng: -73.9980244},
+    zoom: 13,
+    mapTypeControl: false
+  });
+
+  // These are the real estate listings that will be shown to the user.
+  // Normally we'd have these in a database instead.
+
+  var largeInfowindow = new google.maps.InfoWindow();
+
+  // The following group uses the location array to create an array of markers on initialize.
+  for (var i = 0; i < locations.length; i++) {
+    // Get the position from the location array.
+    var position = locations[i].coordinates;
+    var title = locations[i].title;
+    // Create a marker per location, and put into markers array.
+     var marker = new google.maps.Marker({
+      position: position,
+      title: title,
+      animation: google.maps.Animation.DROP,
+      id: i
+    });
+    // Push the marker to our array of markers.
+    markers.push(marker);
+    // Create an onclick event to open an infowindow at each marker.
+    marker.addListener('click', function() {
+      populateInfoWindow(this, largeInfowindow);
+    });
   }
+  document.getElementById('show-listings').addEventListener('click', showListings);
+  document.getElementById('hide-listings').addEventListener('click', hideListings);
+}
 
   // This function populates the infowindow when the marker is clicked. We'll only allow
   // one infowindow which will open at the marker that is clicked, and populate based
