1c8dc098615f1114d5051df9207010c1a37bc1cd
Zyrah Bernardino <zyrahbernardino@gmail.com>
2017-07-20 01:42:00 +1000

    foursquare loading

--- js/app.js
+++ js/app.js
@@ -35,23 +35,16 @@ function MapViewModel() {
     self.filter = ko.observable("All");
     self.Activities = ["All", "Action", "Food", "Rest", "Social", "Events"];
     self.places = places;
-    self.bouncePlace = function(place) {
+    self.selectPlace = function(place) {
        place.marker.setAnimation(4);
        populateInfoWindow(place.marker, self.infoWindow);
        self.map.panTo(place.marker.getPosition());
    }
-    self.filterMap = function (places) {
-      for (var i = 0; i < places.length; i++) {
-        // only have markes in filter
-        // if places[i].type == "All" {
+    self.filterMarkers = ko.computed(function() {
+      console.log(self.filter());
+      hideplaces();
 
-        // }
-        // markers[i].setMap(null);
-      }
-}
-    self.moreInfo = function(place) {
-       // get info from foursquare
-   }
+    });
 }
 
 var mapViewModel = new MapViewModel()
@@ -92,7 +85,7 @@ function initMap() {
       populateInfoWindow(this, largeInfowindow);
     });
   }
-  showplaces();
+  showplaces(mapViewModel.places, "All");
 }
 
 // This function populates the infowindow when the marker is clicked. We'll only allow
@@ -102,10 +95,34 @@ function populateInfoWindow(marker, infowindow) {
   // Check to make sure the infowindow is not already opened on this marker.
   if (infowindow.marker != marker) {
     infowindow.marker = marker;
-    infowindow.setContent('<div>' + marker.title + '</div>' +
-        '<div><input type=\"button\" value=\"More Info\" onclick =' +
-        '\"displayInfo(&quot;'+'&quot;);\"></input></div>'
-      );
+    var ll_str = marker.position.lat() + ',' + marker.position.lng();
+    var id_str = "K4YEDORKJIH5FLLSADFTLBO5V43S0WJ1M3KGCGOL01QQPRTG";
+    var secret_str = "FGFSEHNPR340ONMRYJFV04MF01VCAD5G1CEDVILQKEVP42D5";
+    $.ajax({
+        url: 'https://api.foursquare.com/v2/venues/search',
+        dataType: 'json',
+        data: {
+            client_id: id_str,
+            client_secret: secret_str,
+            limit: 1,
+            ll: ll_str,
+            query: marker.title,
+            v: 20170718,
+            async: true
+        }
+    }).done(function (response) {
+        fsquResponse = response.response.venues[0];
+        // console.log(fsquResponse)
+        infowindow.setContent('<div>' + marker.title + '</div>' +
+        '<div>' + (fsquResponse.location.address===undefined?'':fsquResponse.location.address) + '</div>' +
+        '<div>' + (fsquResponse.contact.phone===undefined?'':fsquResponse.contact.phone) + '</div>' +
+        '<div>' + (fsquResponse.url===undefined?'':fsquResponse.url) + '</div>'
+        );
+    }).fail(function (response, status, error) {
+        infowindow.setContent('<div>' + 'Unable to find information' + '</div>'
+        );
+    });
+
     infowindow.open(map, marker);
     // Make sure the marker property is cleared if the infowindow is closed.
     infowindow.addListener('closeclick', function() {
@@ -114,51 +131,20 @@ function populateInfoWindow(marker, infowindow) {
   }
 }
 
-// Display info from four
-function getInfo(place) {
-  var ll_str = str(place.position.lat) + ',' + str(place.position.lng);
-  var id_str = "K4YEDORKJIH5FLLSADFTLBO5V43S0WJ1M3KGCGOL01QQPRTG";
-  var secret_str = "FGFSEHNPR340ONMRYJFV04MF01VCAD5G1CEDVILQKEVP42D5";
-  var data;
-  $.ajax({
-      url: 'https://api.foursquare.com/v2/venues/search',
-      dataType: 'json',
-      data: 'client_id='+id_str+'&client_secret='+secret_str+'&limit=1&v=20170701%20&ll='+ll_str,
-      async: true,
-  }).done(function (response) {
-      data = response.response.venues;
-      // data.forEach(function (cafe) {
-      //     foursquare = new Foursquare(cafe, map);
-      //     self.cafeList.push(foursquare);
-      // });
-      // self.cafeList().forEach(function (cafe) {
-      //     if (cafe.map_location()) {
-      //         google.maps.event.addListener(cafe.marker, 'click', function () {
-      //             self.selectCafe(cafe);
-      //         });
-      //     }
-      // });
-  }).fail(function (response, status, error) {
-      // $('#query-summary').text('Cafe\'s could not load...');
-  });
-}
 
 // This function will loop through the markers array and display them all.
-function showplaces() {
+function showplaces(places, type) {
   var bounds = new google.maps.LatLngBounds();
   // Extend the boundaries of the map for each marker and display the marker
-  for (var i = 0; i < markers.length; i++) {
-    markers[i].setMap(map);
-    bounds.extend(markers[i].position);
+  for (var i = 0; i < places().length; i++) {
+    p = places()[i]
+    if (p.type == type || type == 'All'){
+      p.marker.setMap(map);
+    }
+    bounds.extend(p.marker.position);
   }
   map.fitBounds(bounds);
 }
-
-// This function will loop through the markers array and display them all.
-function bounceMarker(marker) {
-  marker.setAnimation(google.maps.Animation.BOUNCE);
-}
-
 
 // This function will loop through the listings and hide them all.
 function hideplaces() {
